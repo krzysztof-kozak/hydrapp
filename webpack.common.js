d79b64da4225f3a606cc43426462ecec68429002
Krzysztof Kozak <hithard5@gmail.com>
2020-07-10 22:33:39 +0200

    initial setup completed

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,6 @@ module.exports = {
   /* here you can define another js file */
   entry: {
     index: "./src/js/index.js",
-    another: "./src/js/another.js",
   },
   output: {
     filename: "[name].[hash:8].js",
@@ -67,9 +66,7 @@ module.exports = {
         {
           from: "public",
           globOptions: {
-            ignore: [
-              '**/*.DS_Store'
-            ],
+            ignore: ["**/*.DS_Store"],
           },
         },
       ],
@@ -81,12 +78,6 @@ module.exports = {
       inject: true,
       chunks: ["index"],
       filename: "index.html",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/another.html",
-      inject: true,
-      chunks: ["index", "another"],
-      filename: "another.html",
     }),
   ],
 };
