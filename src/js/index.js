30aaeaca65a4c6572bce1ca2f33a18f13437c719
Krzysztof Kozak <hithard5@gmail.com>
2020-07-10 21:46:46 +0200

    changed index.js and index.html to enable service workers

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
-/* place your code below */
-
-console.log('HELLO 🚀')
+// Enabling PWA:
+import {registerSW} from './pwa.js';
+registerSW();
