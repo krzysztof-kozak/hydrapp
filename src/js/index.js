ed5a2fd94d2d434c825788f1ced6fe1e43b2d49e
Krzysztof Kozak <hithard5@gmail.com>
2020-07-17 17:40:08 +0200

    added more water animations

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ registerSW();
 const counter = document.querySelector(".app__counter--js");
 const addGlass = document.querySelector(".add-button-js");
 const removeGlass = document.querySelector(".remove-button-js");
+const water = document.querySelector(".water");
 
 const key = new Date().toISOString().slice(0, 10);
 let glassCounter = 0;
@@ -16,20 +17,31 @@ if (!localStorage.getItem(key)) {
   localStorage.setItem(key, 0);
 }
 
+if (glassCounter === 0) {
+  console.log(localStorage.getItem(key));
+  water.style.opacity = 0;
+}
+
 const modifyCounter = (operation) => {
   if (operation === "add") {
     glassCounter++;
-  } else {
+    counter.classList.remove("app__counter--animated");
+    water.classList.remove("water--animated-fill");
+    void counter.offsetWidth;
+    void water.offsetWidth;
+    counter.classList.add("app__counter--animated");
+    water.classList.add("water--animated-fill");
+  } else if (operation === "add" && glassCounter > 0) {
     glassCounter--;
+    counter.classList.remove("app__counter--animated");
+    water.classList.remove("water--animated-drain");
+    void counter.offsetWidth;
+    void water.offsetWidth;
+    counter.classList.add("app__counter--animated");
+    water.classList.add("water--animated-drain");
   }
 
-  if (glassCounter < 0) {
-    glassCounter = 0;
-  }
   counter.innerHTML = glassCounter;
-  counter.classList.remove("app__counter--animated");
-  void counter.offsetWidth;
-  counter.classList.add("app__counter--animated");
   localStorage.setItem(key, glassCounter);
 };
 
