517ced9a24b56eb516270358be3b35dc5186d693
Krzysztof Kozak <hithard5@gmail.com>
2020-07-17 15:48:49 +0200

    added increment/decrement function to the buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // Enabling PWA:
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
+
+const counter = document.querySelector(".app__counter--js");
+const addGlass = document.querySelector(".add-button-js");
+const removeGlass = document.querySelector(".remove-button-js");
+
+const key = new Date().toISOString().slice(0, 10);
+let glassCounter = 0;
+counter.innerHTML = localStorage.getItem(key);
+
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0);
+}
+
+const modifyCounter = (operation) => {
+  if (operation === "add") {
+    glassCounter++;
+  } else {
+    glassCounter--;
+  }
+
+  if (glassCounter < 0) {
+    glassCounter = 0;
+  }
+  counter.innerHTML = glassCounter;
+  localStorage.setItem(key, glassCounter);
+};
+
+addGlass.addEventListener("click", () => modifyCounter("add"));
+removeGlass.addEventListener("click", () => modifyCounter("remove"));
